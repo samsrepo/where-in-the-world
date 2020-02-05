9a8b8f85452c97b07c80f31b4e2e9afdd0556b29
Sam Jones <samuelmorrisjones@gmail.com>
2020-02-05 19:05:34 +0000

    Added scroll to top on route change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'top'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
